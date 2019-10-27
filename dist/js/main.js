30c3b45d6b78155f3184ec6fb44448110931497c
eevan7a9 <e7a9@protonmail.com>
2019-10-27 10:13:37 +0900

    added about section and smooth scrolling

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,22 +6,21 @@ const closeModal = function () {
 };
 window.addEventListener("click", function (event) {
   if (event.target == modal_container) {
+    // for Modal
     modal_container.style.display = "none";
   }
 });
-closeModal();
-let navbar_toggled = 0;
-function toggleNavbar(params) {
-  // const nav_container = document.querySelector('.navbar .container');
-  const nav_item_container = document.querySelector('.nav-item-container');
-  // if (!navbar_toggled) {
-  //   nav_container.classList.add('navbar-active');
-  //   nav_item_container.classList.add('nav-item-toggled');
-  //   navbar_toggled = 1
-  // } else {
-  //   nav_container.classList.remove('navbar-active');
-  //   nav_item_container.classList.remove('nav-item-toggled');
-  //   navbar_toggled = 0;
-  // }
-  nav_item_container.classList.toggle('active');
+
+const scrollInto = function (id) {
+  const element = document.querySelector(id);
+  setTimeout(() => {
+    element.scrollIntoView({ behavior: 'smooth', inline: 'center', block: 'center' });
+  }, 500)
+};
+
+const toggleMenu = function () {
+  // for navbar burger menu
+  const nav_item_container = document.querySelector(".nav-item-container");
+  nav_item_container.classList.toggle("active");
+
 }
